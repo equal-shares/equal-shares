07e91fd60ef803397949c381007f3f0a840f93d9
Omer Priel <omer.priel2@gmail.com>
2024-05-19 15:46:55 +0300

    fix-lint

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,7 @@
 # Create the FastAPI application
 
 from contextlib import asynccontextmanager
-from typing import Any, AsyncIterator, Any
+from typing import Any, AsyncIterator
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
