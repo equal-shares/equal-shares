1a57eaf5aeb34824c26b960cd4dcbd571c0a9c1f
Omer Priel <omer.priel2@gmail.com>
2024-05-19 12:55:36 +0300

    temp

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@ set -x #echo on
 cd /app/equal-shares
 
 docker compose -f prod.docker-compose.yaml down
-docker compose -f prod.docker-compose.yaml build
+docker compose -f prod.docker-compose.yaml build --progress=plain --no-cache
