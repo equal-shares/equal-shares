bfae528c4a29b1790213cd70b8814ee852d17bc1
Omer Priel <omer.priel2@gmail.com>
2024-05-19 21:44:39 +0300

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 set -x #echo on
 cd /app/equal-shares
 
+source /app/frontend.env
+
 docker compose -f prod.docker-compose.yaml build --build-arg VITE_API_HOST=$VITE_API_HOST
--- scripts/pull.sh
+++ scripts/pull.sh
@@ -8,7 +8,6 @@ chmod 744 /app/**
 
 # restart and update the services
 source /app/frontend.env
-export $VITE_API_HOST
 
 docker compose -f prod.docker-compose.yaml stop
 docker compose -f prod.docker-compose.yaml build --build-arg VITE_API_HOST=$VITE_API_HOST
