e6cb12beb93249c7dbcf5ab80b0a3f7422afae5d
Omer Priel <omer.priel2@gmail.com>
2024-05-21 14:34:27 +0300

    כןס

--- scripts/rest-env-files.sh
+++ scripts/rest-env-files.sh
@@ -5,3 +5,7 @@ cd /app/equal-shares
 cp /app/equal-shares/prod/backend.env /app/backend.env
 cp /app/equal-shares/prod/db.env /app/db.env
 cp /app/equal-shares/prod/frontend.env /app/frontend.env
+
+chmod 744 /app/backend.env
+chmod 744 /app/db.env
+chmod 744 /app/frontend.env
