36e67fe6ce7993b44fd502ad18425e6fe6f87692
Omer Priel <omer.priel2@gmail.com>
2024-05-21 16:51:31 +0300

    fix

--- backend/src/cli.py
+++ backend/src/cli.py
@@ -24,6 +24,20 @@ def check_database_command() -> None:
     import psycopg_pool
     from psycopg.conninfo import make_conninfo
 
+    print("First test")
+    with psycopg.connect(
+        make_conninfo(
+            "",
+            host=config.pg_host,
+            port=config.pg_port,
+            dbname=config.pg_database,
+            user=config.pg_user,
+            password=config.pg_password,
+        ),
+    ):
+        print("Connected to the database")
+
+
     print("Initializing database...")
     g_pool = psycopg_pool.ConnectionPool(
         make_conninfo(
