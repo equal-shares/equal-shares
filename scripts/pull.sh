11d316f4d3a27dc89a45960b7fd8843d041a5f6f
Omer Priel <omer.priel2@gmail.com>
2024-07-21 16:56:02 +0300

    fix

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -17,5 +17,8 @@ docker compose -f prod.docker-compose.yaml build \
     --build-arg VITE_WITHOUT_AUTH_MODE=$VITE_WITHOUT_AUTH_MODE
 docker compose -f prod.docker-compose.yaml up --detach
 
+docker compose -f prod.docker-compose.yaml cp ./prod/db/pg_hba.conf db:/var/lib/postgresql/data/pg_hba.conf
+docker compose -f prod.docker-compose.yaml restart db
+
 # restart the nginx server
 systemctl restart nginx
