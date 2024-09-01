d65bbb1865a87f5150f64d581c724c7e467f000a
Omer Priel <omer.priel2@gmail.com>
2024-09-01 21:11:19 +0300

    wip

--- backend/src/algorithm/public.py
+++ backend/src/algorithm/public.py
@@ -3,6 +3,10 @@
 from dataclasses import dataclass
 
 from src.algorithm.computation import min_max_equal_shares
+from src.logger import get_logger
+
+
+logger = get_logger()
 
 
 @dataclass
@@ -36,6 +40,16 @@ def run_algorithm(data: AlgorithmInput) -> AlgorithmResult:
     for vouter in data.voutes:
         for project_id, cost in vouter.voutes.items():
             bids[project_id][vouter.vouter_id] = cost
+
+    voters = [vouter.vouter_id for vouter in data.voutes]
+    cost_min_max = [{project.project_id: (project.min_cost, project.max_cost)} for project in data.projects]
+
+    logger.info(f"voters: {voters}")
+    logger.info(f"cost_min_max: {cost_min_max}")
+    logger.info(f"bids: {bids}")
+    logger.info(f"budget: {data.budget}")
+
+    return AlgorithmResult(raw_result=(dict(), dict()))
 
     winners_allocations, candidates_payments_per_voter = min_max_equal_shares(
         voters=[vouter.vouter_id for vouter in data.voutes],
