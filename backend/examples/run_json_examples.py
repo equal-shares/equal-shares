fceb784a31490ce01f7857541c609ca7419af1f2
Erel Segal-Halevi <erelsgl@gmail.com>
2024-10-27 21:06:43 +0200

    use json in examples

---
+++ backend/examples/run_json_examples.py
@@ -0,0 +1,67 @@
+import json, logging
+from src.algorithm.public import PublicEqualSharesInput, PublicEqualSharesResponse, public_equal_shares
+from src.logger import LoggerName, get_logger
+
+from src.algorithm.computation import min_max_equal_shares
+from src.algorithm.average_first import average_first
+from src.algorithm.utils import calculate_average_bids, get_project_min_costs
+
+
+def get_bid_sums(voters: list, bids: dict) -> dict:
+    bid_sums = {}
+    for voter in voters:
+        voter_total_bid = 0
+        for project, project_bids in bids.items():
+            if voter in project_bids:
+                voter_total_bid += project_bids[voter]
+        bid_sums[voter] = voter_total_bid
+    return bid_sums
+
+
+def run_json_example(input_json_path:str, results_json_path:str=None):
+    with open(input_json_path, "r") as f:
+        data_json = json.load(f)
+    # print(data)
+    data = PublicEqualSharesInput(**data_json)
+
+    bid_sums = get_bid_sums(data.voters, data.bids)
+    print("bid sums: ", bid_sums, "\n")
+    
+    project_min_costs = get_project_min_costs(data.cost_min_max)
+
+    averages = calculate_average_bids(data.bids, data.voters)
+    total_allocation = sum([allocation for project, allocation in averages.items()])
+    print(f"\naverages: \nwinners_allocations={averages}\ntotal_allocation={total_allocation}")
+
+    mes_winners, candidates_payments_per_voter = min_max_equal_shares(
+        data.voters, data.cost_min_max, data.budget, data.bids, use_plt=False
+    )
+    total_allocation = sum([allocation for project, allocation in mes_winners.items()])
+    print(f"\nmin_max_equal_shares: \nwinners_allocations={mes_winners}\ntotal_allocation={total_allocation}")
+
+    averagefirst_winners, candidates_payments_per_voter = average_first(
+        data.voters, data.cost_min_max, data.budget, data.bids, use_plt=False
+    )
+    total_allocation = sum([allocation for project, allocation in averagefirst_winners.items()])
+    print(f"\naverage_first: \nwinners_allocations={averagefirst_winners}\ntotal_allocation={total_allocation}")
+
+    print("Project,Minimum,Average,AverageThenMES,MES")
+    for project, avg in averages.items():
+        min_cost = project_min_costs[project]
+        average = averages[project]
+        mes_win = mes_winners[project]
+        avg_win = averagefirst_winners[project]
+        print(f"{project},{min_cost},{mes_win},{average},{avg_win}")
+
+    # res = public_equal_shares(data)
+    # if results_json_path:
+    #     with open(results_json_path, "w") as f:
+    #         json.dump(res.model_dump()["results"], f, indent=2)
+
+
+if __name__=="__main__":
+    get_logger(LoggerName.ALGORITHM).setLevel(logging.INFO)
+    get_logger(LoggerName.ALGORITHM).addHandler(logging.StreamHandler())
+
+    run_json_example("experiment_with_10_council_members.json", "experiment_with_10_council_members_results.json")
+    # run_json_example("experiment_with_200_students.json", "experiment_with_200_students_results.json")
